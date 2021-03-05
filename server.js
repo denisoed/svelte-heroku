9f77aaa4f9b965e7f861ba6374ebe7a8bc121dfd
Denisoed <denisoed@gmail.com>
2021-03-05 23:12:19 +0600

    updated server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static('public'));
+app.use(express.static('dist'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(8080, () => console.log('Server is running on: http://localhost:8080'));
